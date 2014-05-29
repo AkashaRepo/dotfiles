b98b818d6183c638267abd90cb08d231e9196ccc
Nathaniel Sheppard <Nsheppard01@gmail.com>
2014-05-29 13:47:35 -0700

    added documentation.

--- cable.sh
+++ cable.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#Shows the user a diagram and then asks for confirmation.
 echo 'Ensure the leads are connected as follows'
 echo '┎━━━━━━━━━━━━'
 echo '┃R*BWG*******'
 echo '┃************'
 echo is this correct?
-select yn in "Yes" "No"; do
+select yn in "Yes" "No"; do 
     case $yn in
-        Yes ) sudo screen /dev/ttyUSB0 115200; break;;
-        No ) exit;;
+        Yes ) sudo screen /dev/ttyUSB0 115200; break;; #If the user confirms that they have connected the wires, runs the following command.
+        No ) exit;; #Otherwise, the script ends.
     esac
 done
