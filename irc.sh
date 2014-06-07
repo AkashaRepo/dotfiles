74ae614c1cd5d991ff18600b9f1043ad269a8132
Nathaniel Sheppard <Nsheppard01@gmail.com>
2014-06-07 10:08:04 -0700

    using wallterm now, irc.sh does not work.

--- irc.sh
+++ irc.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
-#I got this from https://bbs.archlinux.org/viewtopic.php?id=154094
+#I got this from https://wiki.archlinux.org/index.php/Terminal_as_a_Transparent_Wallpaper and edited it lightly.
 
-#Give a name to the terminal window.
-TITLE="irc"
-#HxW size given in # of charactors. Example: 25x60
-DIMENSIONS=25x60
-#X+Y position given in pixels. Example: 100+200
-POSITION=100+200
-TERMOPTS="--maximize  --hide-borders --hide-toolbar --hide-menubar --title=$TITLE"
-
-if [[ -z $DIMENSIONS || -z $POSITION ]] || [[ -z $TITLE ]] ; then
-	echo "wallterm has not been configured! Please edit this script before running it."
-	exit 1
-fi
-
-xfce4-terminal $TERMOPTS --geometry=$DIMENSIONS+$POSITION &
-#gnome-terminal $TERMOPTS --geometry=$DIMENSIONS+$POSITION &
-
-opt1="-r "$TITLE" -b add,below,sticky"
-opt2="-r "$TITLE" -b add,skip_taskbar,skip_pager"
-
-while true 
-do
-	if [[ "$(wmctrl -l | grep -o "$TITLE")" = $TITLE ]] ; then 
-		wmctrl $opt1 && wmctrl $opt2
-		echo 'done!'
-		break
-	fi
-done
+xfce4-terminal --hide-borders --hide-toolbar --hide-menubar --title=desktopconsole --maximize --command=ssh\ kunoichi@technica.sx & 
+wmctrl -r desktopconsole -b add,below,sticky
+wmctrl -r desktopconsole -b add,skip_taskbar,skip_pager
